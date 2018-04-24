faf0198768d419f7bc32b76f3b31c222d67c3b13
varunmk17 <varunmk17@gmail.com>
2018-04-23 21:39:43 -0400

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectId} = require('mongodb');
 
 const PORT = process.env.PORT || 1331;
 
@@ -30,8 +31,24 @@ app.get('/todos', (req, res) => {
     });
 });
 
+// GET /todos/123123
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid Id');
+    }
+
+    var todo = Todo.findById(id)
+                .then((todo) => {
+                    if(!todo) {
+                        return res.status(404).send('Todo not found');
+                    }
+                    res.status(200).send(todo);
+                }, (e) => res.status(404).send('Oops.!!'))
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running in ${PORT}`)    
+    console.log(`Server running in ${PORT}`)
 });
 
 module.exports = {app};
