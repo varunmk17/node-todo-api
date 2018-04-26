75ea78e6b003ec05b1e98fd1fe9c89923e1d9d3b
varunmk17 <varunmk17@gmail.com>
2018-04-25 22:07:06 -0400

    Add test cases for PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.patch('/todos/:id', (req, res) => {
             if(!todo) {
                 return res.status(404).send();
             }
-            res.send(todo);
+            res.send({todo});
         })
         .catch((e) => res.status(404).send());
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First Todo 1'
 }, {
     _id: new ObjectID(),
-    text: 'Second Todo 1'
+    text: 'Second Todo 2',
+    completed: true,
+    completedAt: 232
 }];
 
 
@@ -110,4 +112,45 @@ describe('DELETE /todos/:id', () => {
     // it('should return 404 if Object Id is invalid', (done) => {
 
     // });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var id = todos[0]._id.toHexString();
+        var text = 'changed text 1';
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                //expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done)
+
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var id = todos[1]._id.toHexString();
+        var text = 'changed text 2';
+
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist;
+            })
+            .end(done)
+    });
 });
