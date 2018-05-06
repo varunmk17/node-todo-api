fe75aadd05509a4ac3dd7f2dbafbeccad16135ce
varunmk17 <varunmk17@gmail.com>
2018-05-05 23:31:44 -0400

    Add token

--- server/server.js
+++ server/server.js
@@ -73,6 +73,7 @@ app.delete('/todos/:id', (req, res) => {
         }, (e) => res.status(404).send('Oops..'));
 });
 
+// UPDATE /todos/123123
 app.patch('/todos/:id', (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
@@ -96,7 +97,18 @@ app.patch('/todos/:id', (req, res) => {
             res.send({todo});
         })
         .catch((e) => res.status(404).send());
+});
 
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => res.status(400).send(err));
 });
 
 
