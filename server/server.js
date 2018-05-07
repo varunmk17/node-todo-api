2e7720059f83a7a198634d46ae48bfd76ce81ab9
varunmk17 <varunmk17@gmail.com>
2018-05-06 22:36:35 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,19 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
+// POST / users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running in ${PORT}`)
 });
